e7600cf42acc189d823ddff7e846725795f007e2
Leonidas <ldbrooks297@gmail.com>
2021-03-05 10:15:38 -0800

    fixed navbar spacing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,23 +18,23 @@ const Navbar = (props) => {
                             <NavLink className="nav-link"  to="/about">About</NavLink>
                         </li>
                     {props.isAuth ? 
-                    <ul>
+                    <>
                         <li className ="nav-item">
                             <NavLink className="nav-link" to="/profile">Profile</NavLink>
                         </li>
                         <li className ="nav-item">
                             <span onClick={props.handleLogout} className="nav-link logout-link">Logout</span>
                         </li>
-                    </ul> 
+                    </> 
                     :
-                    <ul>
+                    <>
                        <li className ="nav-item">
                            <NavLink className="nav-link" to="/signup">Sign up</NavLink>
                        </li>
                        <li className ="nav-item">
                            <NavLink className="nav-link" to="/login">Login</NavLink>
                        </li>
-                   </ul>
+                   </>
                     }
                     </ul>
                 </div>
